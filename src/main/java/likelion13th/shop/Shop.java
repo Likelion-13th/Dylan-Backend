fa1fbb30708b5512de7544114613e10662ffef2d
dh1180 <mjdh1936@gmail.com>
2025-05-29 19:56:06 +0900

    first commit

---
+++ src/main/java/likelion13th/shop/Shop.java
@@ -0,0 +1,24 @@
+package likelion13th.shop;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String maker;
+    private String content;
+    private int price;
+    private String imageUrl;
+
+}
---
+++ src/main/java/likelion13th/shop/ShopRepository.java
@@ -0,0 +1,6 @@
+package likelion13th.shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+}

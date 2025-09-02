3bc64e962782e649ca2270fcdc7a4b180cb19696
dh1180 <mjdh1936@gmail.com>
2025-09-02 22:03:50 +0900

    feat: 주문 도메인/API 정리 및 DTO 리네이밍, 저장소 추가
    - OrderResponseDto → OrderResponse로 리네이밍
    - ItemRepository 신규 추가
    - OrderCreateRequest, OrderResponse, OrderController, Order 엔티티, OrderRepository, OrderService 수정
    - application.yml 설정 업데이트

--- src/main/java/likelion13th/shop/repository/OrderRepository.java
+++ src/main/java/likelion13th/shop/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package likelion13th.shop.repository;
 
-public class OrderRepository {
+import likelion13th.shop.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }

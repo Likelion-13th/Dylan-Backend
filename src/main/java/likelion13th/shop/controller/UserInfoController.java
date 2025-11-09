ccfb124a646532f116a1cc75fbd91a6bdf8ba693
dh1180 <mjdh1936@gmail.com>
2025-11-09 18:54:19 +0900

    fix: user 엔드포인트 오류 수정

--- src/main/java/likelion13th/shop/controller/UserInfoController.java
+++ src/main/java/likelion13th/shop/controller/UserInfoController.java
@@ -3,7 +3,10 @@ package likelion13th.shop.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import likelion13th.shop.DTO.request.AddressRequest;
 import likelion13th.shop.domain.User;
+import likelion13th.shop.global.api.ApiResponse;
+import likelion13th.shop.global.api.SuccessCode;
 import likelion13th.shop.login.auth.jwt.CustomUserDetails;
+import likelion13th.shop.login.service.UserServiceLogin;
 import likelion13th.shop.service.UserAddressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +26,7 @@ public class UserInfoController {
 
     private final UserService userService;
     private final UserAddressService userAddressService;
+    private final UserServiceLogin userServiceLogin;
 
     private Long getCurrentUserId(@AuthenticationPrincipal CustomUserDetails userPrincipal) {
         // userPrincipal에서 현재 로그인한 사용자의 ID를 추출하는 로직
@@ -38,29 +42,30 @@ public class UserInfoController {
     // 내 정보 조회
     @Operation(summary = "내 프로필 조회", description = "현재 로그인한 사용자의 상세 프로필 정보를 조회합니다.")
     @GetMapping("/profile")
-    public ResponseEntity<UserInfoResponse> getMyProfile(@AuthenticationPrincipal CustomUserDetails userPrincipal) {
-        Long userId = getCurrentUserId(userPrincipal);
-        UserInfoResponse response = userService.getUserInfo(userId);
-        return ResponseEntity.ok(response);
+    public ApiResponse<UserInfoResponse> getMyProfile(@AuthenticationPrincipal CustomUserDetails userPrincipal) {
+        User user = userServiceLogin.getAuthenticatedUser(userPrincipal.getProviderId());
+
+        UserInfoResponse userInfo = UserInfoResponse.from(user);
+
+        return ApiResponse.onSuccess(SuccessCode.USER_INFO_GET_SUCCESS, userInfo);
     }
 
     // 내 마일리지 조회
     @Operation(summary = "내 마일리지 조회", description = "현재 로그인한 사용자의 마일리지 정보를 조회합니다.")
     @GetMapping("/mileage")
     public ResponseEntity<UserMileageResponse> getMyMileage(@AuthenticationPrincipal CustomUserDetails userPrincipal) {
-        Long userId = getCurrentUserId(userPrincipal);
-        UserMileageResponse response = userService.getUserMileage(userId);
+        User user = userServiceLogin.getAuthenticatedUser(userPrincipal.getProviderId());
+        UserMileageResponse response = userService.getUserMileage(user.getId());
         return ResponseEntity.ok(response);
     }
 
     // 내 주소 조회
     @Operation(summary = "내 주소 저장", description = "현재 로그인한 사용자의 주소 정보를 생성합니다.")
     @PostMapping("/address")
-    public ResponseEntity<AddressResponse> getMyAddress(@AuthenticationPrincipal CustomUserDetails userPrincipal,
+    public ApiResponse<AddressResponse> getMyAddress(@AuthenticationPrincipal CustomUserDetails userPrincipal,
                                                         @RequestBody AddressRequest request) {
-        String providerId = userPrincipal.getProviderId();
-        AddressResponse response = userAddressService.saveAddress(providerId, request);
-        return ResponseEntity.ok(response);
+        AddressResponse addressResponse = userAddressService.saveAddress(userPrincipal.getProviderId(), request);
+        return ApiResponse.onSuccess(SuccessCode.ADDRESS_SAVE_SUCCESS, addressResponse);
     }
 }
 
